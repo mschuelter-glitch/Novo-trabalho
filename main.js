03805b2158c8683bb18eca41c5f8ffc8e496e18a
mschuelter-glitch <m.schuelter@escola.pr.gov.br>
2026-08-27 09:13:39 -0300

    .

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const caixaPrincipal = document.querySelector(".caixa-principal")
 const caixaPergunta = document.querySelector(".caixa-pergunta")
-const caixaAlternativas = document.querySelector(".caixa-resultado")
+const caixaAlternativas = document.querySelector(".caixa-alternativas")
 const caixaResultado = document.querySelector(".caixa-resultado")
 const textoResultado = document.querySelector(".texto-resultado")
 
